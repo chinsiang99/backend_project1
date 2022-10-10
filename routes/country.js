21d2b40e69a91bac4e0d722ccb32df61d67869a9
chinsiang99 <chinsiang99@gmail.com>
2022-10-11 06:09:25 +0800

    update retrieve country route

--- routes/country.js
+++ routes/country.js
@@ -22,5 +22,38 @@ router.get('/', (req, res, next) => {
         })
 });
 
+/* GET all country. */
+router.get('/:id',
+    check('id', "Invalid route param").isInt(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        connection
+            .raw(`select * from country where id = ?`, req.params.id)
+            .then((result) => {
+                var country = result[0];
+                if (country.length == 0) {
+                    res.json({
+                        message: "no record found"
+                    })
+                } else {
+                    res.json({
+                        country: country
+                    });
+                }
+
+            })
+            .catch((error) => {
+                console.log(error);
+                res.json(500, {
+                    "message": error
+                })
+            })
+    });
+
 
 module.exports = router;
