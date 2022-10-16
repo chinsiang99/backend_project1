177f458a89714385f2c168a961dd2b1380a87303
chinsiang99 <chinsiang99@gmail.com>
2022-10-16 09:27:17 +0800

    add route to insert new country

--- routes/country.js
+++ routes/country.js
@@ -4,7 +4,7 @@ var connection = require('../database.js');
 var sanitizeHtml = require('sanitize-html');
 const { check, validationResult } = require('express-validator');
 
-/* GET all country. */
+/* get all country. */
 router.get('/', (req, res, next) => {
     connection
         .raw(`select * from country`)
@@ -22,7 +22,7 @@ router.get('/', (req, res, next) => {
         })
 });
 
-/* GET all country. */
+/* get specific country with id. */
 router.get('/:id',
     check('id', "Invalid route param").isInt(),
     (req, res, next) => {
@@ -39,6 +39,34 @@ router.get('/:id',
                 if (country.length == 0) {
                     res.json({
                         message: "no record found"
+                    })
+                } else {
+                    res.json({
+                        country: country
+                    });
+                }
+
+            })
+            .catch((error) => {
+                console.log(error);
+                res.json(500, {
+                    "message": error
+                })
+            })
+    });
+
+/* insert new country record */
+router.post('/',
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        connection
+            .raw(`insert into country(country_name) values (?)`, req.body.name)
+            .then((result) => {
+                var country = result[0];
+                if (country.length == 0) {
+                    res.json({
+                        message: "failed to create a new country"
                     })
                 } else {
                     res.json({
