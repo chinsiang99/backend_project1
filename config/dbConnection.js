4653a38094e185512bbea90c6422dbee6d7aecd5
chinsiang99 <chinsiang99@gmail.com>
2022-11-24 06:02:47 +0800

    refactor(dbConnection): renaming variable

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,9 +2,8 @@ const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize({
     dialect: "mysql",
-    //    port: 3306,
-    host: 'student-mysql.ccttwiegufhh.us-east-2.rds.amazonaws.com',
-    database: 'chin_siang',
-    user: 'studentmysql',
-    password: 'studentmysql'
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    user: process.env.USER,
+    password: process.env.PASSWORD
 });
